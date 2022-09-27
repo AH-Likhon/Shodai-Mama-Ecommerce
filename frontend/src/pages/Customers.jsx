81b96eb6bc5c61c2f1d201718ce52a68d681b19c
plcpl-haque <md.haque@profecialinks.com>
2022-09-27 19:15:39 +0600

    customers page designed

--- frontend/src/pages/Customers.jsx
+++ frontend/src/pages/Customers.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+import { customersData, customersGrid } from '../dummy';
+import { Header } from '../components';
 
 const Customers = () => {
     return (
-        <div>Customers</div>
+        <div className='my-20 mx-6 lg:m-8 p-4 lg:p-6 rounded-3xl bg-white'>
+            <Header category='Page' title='Customers' />
+            <GridComponent
+                dataSource={customersData}
+                allowPaging
+                allowSorting
+                toolbar={['Delete']}
+                editSettings={{ allowEditing: true, allowDeleting: true }}
+                width='auto'
+            >
+                <ColumnsDirective>
+                    {
+                        customersGrid.map((item, index) => (
+                            <ColumnDirective key={index} {...item} />
+                        ))
+                    }
+                </ColumnsDirective>
+                <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]} />
+            </GridComponent>
+        </div>
     )
 }
 
